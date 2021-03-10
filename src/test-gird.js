b2a29fec7af24df6c0d06bce261cd3f509f58651
thesmith4734 <thehellhound4734@aim.com>
2021-03-09 23:30:17 -0800

    getting closer

--- src/test-gird.js
+++ src/test-gird.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
 import prog_img from './assets/the_programmer.jpg'
 import voice_img from './assets/the_voice_actor.jpg'
 
-export default function TestGrid() {
-    return(
-        <div className="container">
-            <Grid container direction="row" justify="center" alignItems="center">
-                <Grid item xs={12} xm={6}>
-                    <div className="centered_left">
-                        <img src={prog_img} alt="An extremely good looking man sitting in front of a keyboard looking very smart and good at programming"/>
-                    </div>
+class TestGrid extends Component {
+    render() {
+        return (
+            <div className="index_container">
+                <Grid container>
+                    <Grid item xs={6}>
+                        <div className="centered_left" justify="space-evenly" alignItems="center">
+                            <img src={prog_img} alt="An extremely good looking man sitting in front of a keyboard looking very smart and good at programming"/>
+                            <p>Hi.</p>
+                        </div>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <div className="centered_right">
+                            <img src={voice_img} alt="An extremely good looking man sitting in front of a keyboard looking very smart and good at programming"/>
+                            <p> Hello</p>
+                        </div>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} xm={6}>
-                    <div className="centered_right">
-                        <img src={voice_img} alt="An extremely good looking man sitting in front of a keyboard looking very smart and good at programming"/>
-                    </div>
-                </Grid>
-            </Grid>
-        </div>
-    )
+            </div>
+        )
+    }
 }
+const styles = theme => ({
+    container: {
+        padding: theme.spacing.unit
+    }
+})
+export default withStyles(styles)(TestGrid)
