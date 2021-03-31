f8b57b343a454215151da43ed46773511e10b782
thesmith4734 <thehellhound4734@aim.com>
2021-03-31 16:26:23 -0700

    add title and link sections

--- src/programmer-page.js
+++ src/programmer-page.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { withStyles, makeStyles } from "@material-ui/core/styles";
 import Divider from '@material-ui/core/Divider';
 import Introduction from "./components/introduction";
-import Projects from "./components/projects";
+import ProjectCarousel from "./components/project-carousel";
+import Title from "./components/title";
+import LinkList from "./components/link-list";
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -12,13 +15,26 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
+const projectTitle = {
+        title: "Projects",
+    }
+
+const linksTitle = {
+    title: "Links",
+}
+
 export default function ProgrammerPage() {
     const classes = useStyles();
     return (
         <div className={classes.root}>
             <Introduction />
             <Divider variant="middle" />
-            <Projects />
+            <Title item={projectTitle}/>
+            <ProjectCarousel />
+            <Divider variant="middle" />
+            <Title item={linksTitle} />
+            <LinkList />
+
         </div>
     )
 }
