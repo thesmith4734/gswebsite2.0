317accd77c09ab25a6a7d9a49bea6778be220da4
thesmith4734 <thehellhound4734@aim.com>
2021-04-05 20:29:12 -0700

    remove space

--- src/MyTheme.js
+++ src/MyTheme.js
@@ -3,7 +3,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export default createMuiTheme({
     palette: {
         primary: {
-            main: '#2B4162 '
+            main: '#2B4162'
         },
         secondary: {
             main: '#385F71'
