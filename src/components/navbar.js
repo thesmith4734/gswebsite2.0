2a00024aea5727e0c168f477222107815d69aa3e
thesmith4734 <thehellhound4734@aim.com>
2021-04-06 22:45:02 -0700

    Add buttons to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -4,9 +4,11 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
+import ButtonBase from '@material-ui/core/ButtonBase';
 import IconButton from '@material-ui/core/IconButton';
 import HomeIcon from '@material-ui/icons/Home';
 import SettingsIcon from '@material-ui/icons/Settings';
+import { useHistory } from 'react-router-dom';
 
 
 
@@ -20,30 +22,57 @@ const useStyles = makeStyles((theme) => ({
     },
     settings: {
         marginLeft: 0
+    },
+    button: {
+        width: '100%',
+        height: '100%'
     }
 }));
 
 export default function Introduction() {
     const classes = useStyles();
+    const history = useHistory();
+
+    const handleHome = () => {
+        history.push('/');
+    }
+
+    const handleProg = () => {
+        history.push('/the_programmer');
+    }
+
+    const handleVoice = () => {
+        console.log('Not Implemented')
+    }
+
+    const handleSettings = () => {
+        console.log('Not Implemented');
+    }
+
     return (
         <div className={classes.root}>
             <AppBar color="secondary">
                 <Toolbar color="secondary">
-                    <IconButton>
+                    <IconButton onClick={ handleHome }>
                         <HomeIcon color="inherit" />
                     </IconButton>
                     <Divider orientation="vertical" variant="middle" flexItem className={classes.divider}/>
-                    <Typography variant="h8" className={classes.title} >
-                        The Programmer
-                    </Typography>
+                    <ButtonBase onClick={ handleProg }>
+                        <Typography variant="h8" className={classes.title}>
+                            The Programmer
+                        </Typography>
+                    </ButtonBase>
                     <Divider orientation="vertical" variant="middle" flexItem className={classes.divider}/>
-                    <Typography variant="h8" className={classes.title} >
-                        The Voice Actor
-                    </Typography>
+                    <ButtonBase onClick={ handleVoice }>
+                        <Typography variant="h8" className={classes.title} onClick={ handleVoice }>
+                            The Voice Actor
+                        </Typography>
+                    </ButtonBase>
                     <Divider orientation="vertical" variant="middle" flexItem className={classes.divider}/>
-                    <IconButton className={classes.settings}>
+                    <IconButton className={classes.settings} onClick={ handleSettings }>
                         <SettingsIcon color="inherit" />
                     </IconButton>
+                    <Divider orientation="vertical" variant="middle" flexItem className={classes.divider}/>
                 </Toolbar>
             </AppBar>
         </div>
