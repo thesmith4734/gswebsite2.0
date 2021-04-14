d26cbd69ac2bfe3aedb55c277b3b64d124988e00
thesmith4734 <thehellhound4734@aim.com>
2021-04-14 13:05:08 -0700

    remove div

--- src/components/title.js
+++ src/components/title.js
@@ -5,15 +5,9 @@ import Typography from '@material-ui/core/Typography';
 
 
 const useStyles = makeStyles((theme) => ({
-    background: {
-        margin: '0',
-        padding: '0',
-        width: '100%',
-        position: 'relative',
-    },
     titlecard: {
         width: '60%',
-        height: '20%',
+        height: '100px',
         marginTop: theme.spacing(7),
         display: "flex",
         flexDirection: "column",
@@ -26,10 +20,8 @@ const useStyles = makeStyles((theme) => ({
 export default function Title(props) {
     const classes = useStyles();
     return (
-        <div className={classes.background}>
             <Card className={classes.titlecard}>
                 <Typography variant="h2">{props.item.title}</Typography>
             </Card>
-        </div>
     )
 }
